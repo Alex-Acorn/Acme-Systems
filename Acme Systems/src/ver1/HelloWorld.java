411cd1db4de9e6ec960163190ab6e892a19b3089
Alex Acorn <acorn.alex@gmail.com>
2021-08-29 10:57:26 -0400

    Commit whatFor method

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -11,5 +11,8 @@ public class HelloWorld {
 		String Msg = "Hey Now!";
 		System.out.println(Msg);
 	}
-
+	public static void whatFor(String a) {
+		String Msg = a + ",What for?";
+		System.out.println(Msg);
+	}
 }
